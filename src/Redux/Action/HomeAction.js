6be34da40316a46e66b47ce49e2e694967c40778
bayucucanh <58686350+bayucucanh@users.noreply.github.com>
2022-06-06 13:57:29 +0700

    Add Detail Action

--- src/Redux/Action/HomeAction.js
+++ src/Redux/Action/HomeAction.js
@@ -1,21 +1,21 @@
-import axios from 'axios';
-import {GET_DATA} from '../Types';
-import {baseURL} from '../../Service';
+// import axios from 'axios';
+// import {GET_DATA} from '../Types';
+// import {baseURL} from '../../Service';
 
-export const getDataPokemon = data => ({
-  type: GET_DATA,
-  payload: data,
-});
+// export const getDataPokemon = data => ({
+//   type: GET_DATA,
+//   payload: data,
+// });
 
-export function GetDataPokemon() {
-  return async dispatch => {
-    await axios
-      .get(baseURL)
-      .then(async response => {
-        dispatch(getDataPokemon(response.data));
-      })
-      .catch(error => {
-        console.log(error);
-      });
-  };
-}
+// export function GetDataPokemon() {
+//   return async dispatch => {
+//     await axios
+//       .get(baseURL)
+//       .then(async response => {
+//         dispatch(getDataPokemon(response.data));
+//       })
+//       .catch(error => {
+//         console.log(error);
+//       });
+//   };
+// }
