eea42bd77a2b1608893a1e29eb17fcb18d5cc55b
bayucucanh <58686350+bayucucanh@users.noreply.github.com>
2022-06-06 12:42:38 +0700

    Fixe code error

--- src/Redux/Action/index.js
+++ src/Redux/Action/index.js
@@ -16,44 +16,45 @@ export const registerSucces = data => ({
   payload: data,
 });
 
-export const loginSucces = (data) => ({
+export const loginSucces = data => ({
   type: LOGIN,
-  payload: (data),
+  payload: data,
 });
 
-export const getDataPokemon = (data) => ({
+export const getDataPokemon = data => ({
   type: GET_DATA,
   payload: data,
 });
 
-export const getDetailPokemon = (data) => ({
+export const getDetailPokemon = data => ({
   type: GET_DETAIL,
   payload: data,
 });
 
-export const loading = (value) => ({
+export const loading = value => ({
   type: LOADING,
   payload: value,
 });
 
-export const refresh = (value) => ({
+export const refresh = value => ({
   type: REFRESH,
   payload: value,
 });
 
-export const nextPage = (value) => ({
+export const nextPage = value => ({
   type: NEXT_PAGE,
   payload: value,
 });
-export const prevPage = (value) => ({
+export const prevPage = value => ({
   type: PREV_PAGE,
   payload: value,
 });
 
-export const GetDataPokemon = () => async (dispatch) => {
+export function GetDataPokemon() {
+  return async dispatch => {
     dispatch(loading(true));
     try {
-      await axios.get(baseURL).then((response) => {
+      await axios.get(baseURL).then(response => {
         dispatch(loading(false));
         dispatch(refresh(false));
         dispatch(getDataPokemon(response.data));
@@ -62,43 +63,50 @@ export const GetDataPokemon = () => async (dispatch) => {
       console.log(error);
     }
   };
+}
 
-export const GetDataPokemonDetail = (url, navigation) => async (dispatch) => {
-  dispatch(loading(true));
-  try {
-    await axios.get(url).then((response) => {
-      dispatch(loading(false));
-      dispatch(refresh(false));
-      dispatch(getDetailPokemon(response.data));
-      navigation.navigate('DetailScreen');
-    });
-  } catch (error) {
-    console.log(error);
-  }
-};
+export function GetDataPokemonDetail(url, navigation) {
+  return async dispatch => {
+    dispatch(loading(true));
+    try {
+      await axios.get(url).then(response => {
+        dispatch(loading(false));
+        dispatch(refresh(false));
+        dispatch(getDetailPokemon(response.data));
+        navigation.navigate('DetailScreen');
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+}
 
-export const GetDataAfterNext = (url) => async dispatch => {
-  dispatch(loading(true));
-  try {
-    await axios.get(url).then(response => {
-      dispatch(loading(false));
-      dispatch(refresh(false));
-      dispatch(getDataPokemon(response.data));
-    });
-  } catch (error) {
-    console.log('error');
-  }
-};
+export function GetDataAfterNext(url) {
+  return async dispatch => {
+    dispatch(loading(true));
+    try {
+      await axios.get(url).then(response => {
+        dispatch(loading(false));
+        dispatch(refresh(false));
+        dispatch(getDataPokemon(response.data));
+      });
+    } catch (error) {
+      console.log('error');
+    }
+  };
+}
 
-export const GetDataAfterPrevious = (url) => async dispatch => {
-  dispatch(loading(true));
-  try {
-    await axios.get(url).then(response => {
-      dispatch(loading(false));
-      dispatch(refresh(false));
-      dispatch(getDataPokemon(response.data));
-    });
-  } catch (error) {
-    console.log('error');
-  }
-};
+export function GetDataAfterPrevious(url) {
+  return async dispatch => {
+    dispatch(loading(true));
+    try {
+      await axios.get(url).then(response => {
+        dispatch(loading(false));
+        dispatch(refresh(false));
+        dispatch(getDataPokemon(response.data));
+      });
+    } catch (error) {
+      console.log('error');
+    }
+  };
+}
