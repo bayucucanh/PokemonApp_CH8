f49abaeb8615e8bec0f08f0ae0398f85297314bd
bayucucanh <58686350+bayucucanh@users.noreply.github.com>
2022-06-06 17:55:30 +0700

    Add firebase

--- src/Redux/Action/HomeAction.js
+++ src/Redux/Action/HomeAction.js
@@ -1,45 +1,45 @@
-// import axios from 'axios';
-// import {GET_DATA} from '../Types';
-// import {baseURL} from '../../Service';
+import axios from 'axios';
+import {GET_DATA} from '../Types';
+import {baseURL} from '../../Service';
 
-// export const getDataPokemon = data => ({
-//   type: GET_DATA,
-//   payload: data,
-// });
+export const getDataPokemon = data => ({
+  type: GET_DATA,
+  payload: data,
+});
 
-// export function GetDataPokemon() {
-//   return async dispatch => {
-//     await axios
-//       .get(baseURL)
-//       .then(async response => {
-//         dispatch(getDataPokemon(response.data));
-//       })
-//       .catch(error => {
-//         console.log(error);
-//       });
-//   };
-// }
+export function GetDataPokemon() {
+  return async dispatch => {
+    await axios
+      .get(baseURL)
+      .then(async response => {
+        dispatch(getDataPokemon(response.data));
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  };
+}
 
-// export const GetDataAfterNext = url => {
-//   console.log('next', url);
-//   return async dispatch => {
-//     try {
-//       await axios.get(url).then(response => {
-//         dispatch(getDataPokemon(response.data));
-//       });
-//     } catch (error) {
-//       console.log('error');
-//     }
-//   };
-// };
-// export const GetDataAfterPrevious = url => {
-//   return async dispatch => {
-//     try {
-//       await axios.get(url).then(response => {
-//         dispatch(getDataPokemon(response.data));
-//       });
-//     } catch (error) {
-//       console.log('error');
-//     }
-//   };
-// };
+export const GetDataAfterNext = url => {
+  console.log('next', url);
+  return async dispatch => {
+    try {
+      await axios.get(url).then(response => {
+        dispatch(getDataPokemon(response.data));
+      });
+    } catch (error) {
+      console.log('error');
+    }
+  };
+};
+export const GetDataAfterPrevious = url => {
+  return async dispatch => {
+    try {
+      await axios.get(url).then(response => {
+        dispatch(getDataPokemon(response.data));
+      });
+    } catch (error) {
+      console.log('error');
+    }
+  };
+};
